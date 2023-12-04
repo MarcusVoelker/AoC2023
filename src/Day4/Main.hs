b25c0b54deab72c572d7e738cb3bd2ece4e65de7
Marcus Völker <voelker@embedded.rwth-aachen.de>
2023-12-04 12:05:53 +0100

    Day 4

--- src/Day4/Main.hs
+++ src/Day4/Main.hs
@@ -1,6 +1,44 @@
 module Day4.Main where
 
+import Data.List
+import qualified Data.Map as M
+import Data.Maybe
 import Harness
+import Text.Parsec
+import Text.Parsec.Token
+
+type Card = (Int, [Int], [Int])
+
+card :: Stream s m Char => ParsecT s u m Card
+card = do
+  string "Card"
+  many1 space
+  i <- read <$> many1 digit
+  string ":"
+  many space
+  a <- many1 (do d <- read <$> many1 digit; many1 space; return d)
+  many space
+  string "|"
+  many space
+  b <- sepBy1 (read <$> many1 digit) (many1 (string " "))
+  return (i, a, b)
+
+cards :: Stream s m Char => ParsecT s u m [Card]
+cards = sepBy1 card (many space)
+
+p1 :: [Card] -> Int
+p1 cs = sum $ [2 ^ (l -1) | (i, a, b) <- cs, let l = length (a `intersect` b), l > 0]
+
+addCards :: M.Map Int Int -> (Int, Int) -> M.Map Int Int
+addCards m (i, c) =
+  let n = m M.! i
+   in foldr (M.adjust (+ n)) m [i + 1 .. i + c]
+
+p2 :: [Card] -> Int
+p2 cs =
+  let ns = map (\(i, a, b) -> (i, length (a `intersect` b))) cs
+      m = foldl addCards (M.fromList (map (\(i, _) -> (i, 1)) ns)) ns
+   in M.foldr (+) 0 m
 
 main :: Bool -> IO ()
-main _ = putStrLn "Day not implemented yet"
+main b = runParse 4 b cards p1 p2
