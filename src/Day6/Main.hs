67e134a8b0dc8daf967ea6026ec40db2426ab4ba
Marcus Völker <voelker@embedded.rwth-aachen.de>
2023-12-06 11:18:59 +0100

    Day 6

--- src/Day6/Main.hs
+++ src/Day6/Main.hs
@@ -2,5 +2,29 @@ module Day6.Main where
 
 import Harness
 
+rawData :: [(Integer, Integer)]
+rawData = [(44, 208), (80, 1581), (65, 1050), (72, 1102)]
+
+rawData2 :: (Integer, Integer)
+rawData2 = (44806572, 208158110501102)
+
+count :: (Integer, Integer) -> Integer
+count (a, b) = fromIntegral $ length $ filter (> b) $ map (\h -> h * (a - h)) [0 .. a]
+
+goodCount :: (Integer, Integer) -> Integer
+goodCount (a, b) =
+  let fa = fromIntegral a
+      fb = fromIntegral b
+      ph = fa / 2
+      min = floor (ph - sqrt (ph * ph - fb) + 1)
+      max = ceiling (ph + sqrt (ph * ph - fb) - 1)
+   in max - min + 1
+
+p1 :: Integer
+p1 = product $ map goodCount rawData
+
+p2 :: Integer
+p2 = goodCount rawData2
+
 main :: Bool -> IO ()
-main _ = putStrLn "Day not implemented yet"
+main b = print $ if b then p2 else p1
