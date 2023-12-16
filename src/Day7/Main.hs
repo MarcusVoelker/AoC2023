79c6cd88eca9fd54e6d8f651dd4d243d50f589e1
Marcus Völker <voelker@embedded.rwth-aachen.de>
2023-12-16 09:39:02 +0100

    Old D7/D8-1

--- src/Day7/Main.hs
+++ src/Day7/Main.hs
@@ -1,6 +1,72 @@
 module Day7.Main where
 
+import Data.Char
+import Data.List
+import Data.Ord
 import Harness
 
+data HandType = High | One | Two | Three | Full | Four | Five deriving (Eq, Show, Ord)
+
+handType :: String -> HandType
+handType s =
+  let so = group $ sort s
+   in case length so of
+        1 -> Five
+        2 -> if length (head so) `elem` [2, 3] then Full else Four
+        3 -> case sort (map length so) of
+          [1, 1, 3] -> Three
+          [1, 2, 2] -> Two
+        4 -> One
+        5 -> High
+
+repJ :: String -> Char -> String
+repJ ('J' : r) c = c : repJ r c
+repJ (h : r) c = h : repJ r c
+repJ [] _ = []
+
+handType2 :: String -> HandType
+handType2 s = maximum $ map (handType . repJ s) "23456789TQKA"
+
+charNum :: Char -> Int
+charNum 'A' = 14
+charNum 'K' = 13
+charNum 'Q' = 12
+charNum 'J' = 11
+charNum 'T' = 10
+charNum c = digitToInt c
+
+charComp :: Char -> Char -> Ordering
+charComp = comparing charNum
+
+charNum2 :: Char -> Int
+charNum2 'J' = 0
+charNum2 c = charNum c
+
+charComp2 :: Char -> Char -> Ordering
+charComp2 = comparing charNum2
+
+lexCompBy :: (a -> a -> Ordering) -> [a] -> [a] -> Ordering
+lexCompBy o [] [] = EQ
+lexCompBy o (c1 : l1) (c2 : l2)
+  | o c1 c2 == EQ = lexCompBy o l1 l2
+  | otherwise = o c1 c2
+
+handSort :: (String -> HandType) -> (Char -> Char -> Ordering) -> String -> String -> Ordering
+handSort t c h1 h2
+  | t h1 /= t h2 = compare (t h1) (t h2)
+  | otherwise = lexCompBy c h1 h2
+
+p1 :: String -> Integer
+p1 s =
+  let dat = map (\l -> (take 5 l, read $ drop 6 l)) $ lines s
+      sdat = sortBy (\a b -> handSort handType charComp (fst a) (fst b)) dat
+   in sum $ zipWith (\s (_, n) -> s * n) [1 ..] sdat
+
+p2 :: String -> Integer
+p2 s =
+  let dat = map (\l -> (take 5 l, read $ drop 6 l)) $ lines s
+      sdat = sortBy (\a b -> handSort handType2 charComp2 (fst a) (fst b)) dat
+   in sum $ zipWith (\s (_, n) -> s * n) [1 ..] sdat
+
 main :: Bool -> IO ()
-main _ = putStrLn "Day not implemented yet"
+main b = run 7 b p1 p2
--- src/Day8/Main.hs
+++ src/Day8/Main.hs
@@ -1,6 +1,46 @@
 module Day8.Main where
 
+import Control.Monad
+import Data.List
+import qualified Data.Map as M
 import Harness
+import Text.Parsec
+import Text.Parsec.Token
+
+instr :: (Stream s m Char) => ParsecT s u m ([Bool], M.Map String (String, String))
+instr = do
+  l <- many1 ((char 'L' >> return False) <|> (char 'R' >> return True))
+  spaces
+  ss <- many1 $ do
+    s1 <- many1 letter
+    string " = ("
+    s2 <- many1 letter
+    string ", "
+    s3 <- many1 letter
+    string ")"
+    many space
+    return (s1, (s2, s3))
+  return (l, M.fromList ss)
+
+p1 :: ([Bool], M.Map String (String, String)) -> Int
+p1 (is, rs) = length $ takeWhile (/= "ZZZ") $ scanl (\c b -> (if b then snd else fst) (rs M.! c)) "AAA" (cycle is)
+
+p2 :: ([Bool], M.Map String (String, String)) -> Int
+p2 (is, rs) = length $ takeWhile (any ((/= 'Z') . last)) $ scanl (\cs b -> cs >>= \c -> [(if b then snd else fst) (rs M.! c)]) (filter (\k -> last k == 'A') $ M.keys rs) (cycle is)
+
+p1D :: ([Bool], M.Map String (String, String)) -> IO ()
+p1D (is, rs) = mapM_ print $ scanl (\c b -> (if b then snd else fst) (rs M.! c)) "AAA" (cycle is)
+
+orbit :: ([Bool], M.Map String (String, String)) -> (Int, String) -> [(Int, String)]
+orbit (is, rs) (i0, s0) =
+  let trans (idx, k) = (mod (idx + 1) (length is), (if is !! idx then snd else fst) (rs M.! k))
+   in iterate trans (i0, s0)
+
+p2D :: ([Bool], M.Map String (String, String)) -> IO ()
+p2D (is, rs) = print $ orbit (is, rs) (0, "AAA")
 
 main :: Bool -> IO ()
-main _ = putStrLn "Day not implemented yet"
+main b = runParse 8 b instr p1 p2
+
+mainDebug :: Bool -> IO ()
+mainDebug b = debugParse 8 b instr p1D p2D
